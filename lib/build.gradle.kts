f1218f766a8f2162178bd59647a92faa59304db4
Vladislav Maslow <masloff@zoho.com>
2023-11-30 03:59:09 +0300

    change build kotlin and java version to 11

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -111,11 +111,13 @@ java {
         languageVersion.set(JavaLanguageVersion.of(11))
     }
 }
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-	jvmTarget = "1.8"
+	jvmTarget = "11"
 }
+
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-	jvmTarget = "1.8"
+	jvmTarget = "11"
 }
